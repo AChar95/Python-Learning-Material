23ea7f3c80d88cd48b465d999e8ffc18cc78f01f
AChar95 <allancharles95@outlook.com>
2021-04-10 21:28:33 +0100

    added hello world

---
+++ Hello-World/hello-world.py
@@ -0,0 +1,14 @@
+# This script prints the words "Hello World"
+
+print ("Hello World")
+
+# This is a comment
+# This is another comment
+# This is yet another comment
+
+'''
+This is the easy way of doing a multi-line comment
+This is a comment
+This is another comment
+This is yet another comment
+'''
