e9e8b5bcc5c7778f3211c51961e7f9fc21896d9b
AChar95 <allancharles95@outlook.com>
2021-04-11 01:26:16 +0100

    added variables script, added gitignore

---
+++ Variables/variables.py
@@ -0,0 +1,27 @@
+#Variables can be assigned
+
+userAge = 30
+
+userAge, userName = 30, 'Peter'
+
+x = 5
+y = 10 
+# x = y
+
+print( "x = ", x)
+print("y = ", y)
+
+# Python supports the standard mathmatical operators
+
+x = 6
+y = 3
+
+print( x + y ) # 9
+print( x * y)  # 18
+print( x - y)  # 3
+print( x / y)  # 2
+
+x = x + 2
+print(x) # 8
+y += 2
+print(y) # 5
