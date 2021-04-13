ac1816f6755aa3d4665fd2dd6b4bc3fec6bdeb8c
AChar95 <allancharles95@outlook.com>
2021-04-13 21:31:34 +0100

    added default-parameters lesson

---
+++ Functions/default-parameters.py
@@ -0,0 +1,31 @@
+def someFunction(a, b, c=1, d=2, e=3): # formal parameters
+    print(a, b, c, d, e)
+
+
+'''    
+Here the function cannot exist as parameters with a default value should be last:
+def someIncorrectFunction(p, q=1, r):
+    print(p, q, r)
+def someCorrectFunction(p, r, q=1):
+    print(p, q, r)
+'''
+someFunction(10, 20)
+
+def addNumbers(*num): # takes in a random amount of values (non-keyword variables)
+    sum = 0
+    for i in num:
+        sum += i
+    print(sum)
+
+addNumbers(1,2,3,4,5)
+
+def printMemberAge(**age): # This allows you to take in a random amount of key=value pairs (keyword variables)
+    for i, j in age.items():
+        print("Name = %s, Age = %s" %(i,j))
+
+printMemberAge(Peter = 30, Jon = 90)
+
+'''
+When a function can takin formal, nonkeyworded and keyword variables
+Then the following must be used:
+def someFunction(farg, *args, **kwargs):
