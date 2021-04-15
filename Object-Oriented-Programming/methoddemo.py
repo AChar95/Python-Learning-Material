70acc0a70771ef099565aff3128a1446b15fd4a4
AChar95 <allancharles95@outlook.com>
2021-04-15 22:01:26 +0100

    added example on how class and static method works

---
+++ Object-Oriented-Programming/methoddemo.py
@@ -0,0 +1,13 @@
+class MethodDemo:
+    
+    a = 1
+    
+    @classmethod
+    def classM(cls):
+        print("Class Method. cls.a = ", cls.a)
+    
+    @staticmethod
+    def staticM():
+        print("Static Method")
+        
+MethodDemo.classM()
