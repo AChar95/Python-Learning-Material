da4bf9365d1b741a0d9b21b13f43b603e045a733
AChar95 <allancharles95@outlook.com>
2021-04-22 20:40:15 +0100

    updated gametasks scores variable and added logic to remove txt and rename temp file

--- Projects/Maths-and-Binary/gametask.py
+++ Projects/Maths-and-Binary/gametask.py
@@ -5,33 +5,37 @@ def printInstructions(instruction):
 
 def getUserScore(userName):
     try:
-        scores = open('userScores.txt', 'r')
-        for line in scores:
+        scoresFile = open('userScores.txt', 'r')
+        for line in scoresFile:
             content = line.split(", ")
             if (content[0] == userName):
-                scores.close()
+                scoresFile.close()
                 return content[1]
-            scores.close()
+            scoresFile.close()
             return "-1"
     except IOError:
         print("File userScores.txt cannot be found, creating new file")
-        scores = open('userScores.txt', 'w')
-        scores.writelines(["Ann, 100", "\nBenny, 102", "\nCarol, 214", "\nDarren, 129"])
-        scores.close()
+        scoresFile = open('userScores.txt', 'w')
+        scoresFile.writelines(["Ann, 100", "\nBenny, 102", "\nCarol, 214", "\nDarren, 129"])
+        scoresFile.close()
         return "-1"
 
 def updateUserScore(newUser, userName, score):
-    if (newUser == True):
-        scores = open('userScores.txt', 'a')
-        scores.write("\n%s, %s" %(userName, score))
-        scores.close()
+    if newUser == True:
+        scoresFile = open('userScores.txt', 'a')
+        scoresFile.write("\n%s, %s" %(userName, score))
+        scoresFile.close()
     else:
-        scores = open('userScores.txt', 'r')
+        scoresFile = open('userScores.txt', 'r')
         tempFile = open(('userScores.tmp', 'w'))
-        for line in scores:
+        for line in scoresFile:
             content = line.split(", ")
-            if (content[0] == userName):
-                content[1] = score
-            scores.writeline([content[0], ", " content[1]])
-        scores.close()
+            if content[0] == userName:
+                tempFile.write(userName + ", " + score + "\n")
+            else:
+                tempFile.write(line)
+            
+        scoresFile.close()
         tempFile.close()
+        remove('userScores.txt')
+        rename('userScores.tmp', 'userScores.txt')
