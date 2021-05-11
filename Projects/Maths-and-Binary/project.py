da6dcb02a1985d99bff1643170bc4ba12e7ae2d2
AChar95 <allancharles95@outlook.com>
2021-05-11 22:28:19 +0100

    updated with game display logic

--- Projects/Maths-and-Binary/project.py
+++ Projects/Maths-and-Binary/project.py
@@ -9,7 +9,7 @@ try:
     Your task is to convert this value into base 2.
     Each correct answer gives you one point.
     No points are deducted for wrong answers.'''
-    
+
     bg = BinaryGame()
     mg = MathsGame()
     userName = input("Please enter your name: ")
@@ -19,12 +19,26 @@ try:
         score = 0
     else:
         newUser = False
-    print("Hello %s, your score is %i" %(userName, score))
+    print("Hello %s, your score is %i" % (userName, score))
     userChoice = 0
     while userChoice != "-1":
         game = input("Please pick a game: Maths Game (1) or Binary Game (2): ")
         if game != "1" or game != "2":
             print("You have not selected a valid game type")
-            
-        
+        while True:
+            try:
+                numQuestions = input("Please select the number of questions between 1 to 10")
+                num = int(numQuestions)
+                break
+            except:
+                print("The value %s is not a valid number please try again")
+                numQuestions = input("Please select a valid number of questions between 1 to 10")
+        if game == "1":
+            mg.noOfQuestions = num
+            printInstructions(mathsInstructions)
+            score = score + mg.generateQuestions()
+        else:
+            bg.noOfQuestions = num
+            printInstructions(binaryInstructions)
+            score = score + bg.generateQuestions()
 except Exception as e:
