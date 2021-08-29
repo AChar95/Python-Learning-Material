7e4311be7f7d72fa0d4ba6b319751f4534a05adb
AChar95 <allancharles95@outlook.com>
2021-08-29 22:33:35 +0100

    fixed to include check for newl;y added user

--- CSV-files/write-csv.py
+++ CSV-files/write-csv.py
@@ -1,8 +1,14 @@
 import csv
 
 newRow = ["1004","Neil", "Harris", "HR"]
-file = open("example.csv", "a")
+file = open("example.csv", "a", newline="")
 
 wrt = csv.writer(file)
 wrt.writerow(newRow)
 file.close()
+file = open("example.csv", "r",newline="")
+rdr = csv.reader(file)
+for line in rdr:
+    if line[0] == "1004":
+        print("Neil Harris added to the csv")
+file.close
