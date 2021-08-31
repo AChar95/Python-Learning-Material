2317f600cdef8d57c6098686825cfada021d0a20
AChar95 <allancharles95@outlook.com>
2021-08-31 17:52:34 +0100

    adding os module calls

--- os-module/os-module.py
+++ os-module/os-module.py
@@ -6,3 +6,11 @@ os.chdir("/users/allan/desktop")
 print(os.getcwd())
 
 os.makedirs("python test folder") # this creates "python test folder on the desktop"
+
+os.path.isfile("test.docx") # Checks to see if the file test.docx exists
+
+os.path.isdir("python test folder") # Checks to see if the directory exists
+
+os.remove("test.docx") # Removes file called test.docx doesn't remove folders
+
+os.removedirs("python test folder") # removes folder
