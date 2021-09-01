918105453c5eead29a27ab3b9cd3b101e29e5281
AChar95 <allancharles95@outlook.com>
2021-09-01 21:00:41 +0100

    updated to include new magic 8 ball class

---
+++ Projects/Magic-8-Ball-(Class)/magic8ball.py
@@ -0,0 +1,33 @@
+import os, sys, random as rand
+
+
+class magic8ball:
+    def __init__(self, name):
+        self.name = name
+        self.__mQuestions = []
+    
+    def __startGame(self):
+        print("Hello " + self.name)
+        uQuestion = True
+        answers = ("IT IS CERTAIN", "YOU MAY RELY ON IT", "AS i SEE IT, YES", "OUTLOOK LOOKS GOOD", "MOST LIKELY", "IT IS DECIDEDLY SO", "WITHOUT A DOUBT", "YES DEFINITELY")
+        while  uQuestion:
+            userQuestion = input("Please ask a question: ")
+            if len(userQuestion) > 0:
+                self.__mQuestions.append(userQuestion)
+                print(answers[rand.randint(0,len(answers)-1)])
+            else:
+                print("Thank you for playing")
+                sys.exit
+    
+    def __writeQuestions(self):
+        try:
+            fileName = self.name + ".csv"
+            testFile = os.path.isfile(fileName +".csv")
+            if testFile == False:
+                print("Creating your Question file")    
+            file = open(fileName, "a+")
+            
+            
+        except IOError:
+            newFile = open(fileName, 'w')
+            
